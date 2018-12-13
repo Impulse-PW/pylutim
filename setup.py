9f679659357ab999a6ac2b5716dc68358bcb3946
Piero-Bird <pierobird@gmail.com>
2018-12-13 10:20:20 -0700

    Update imports and add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+import os
+import setuptools
+
+setuptools.setup(
+    name="pylutim",
+    version="0.5",
+    description="API wrapper for Lutim (Let's Upload That IMage)",
+    install_requires=["validators"],
+    author="Impulse-PW",
+    url="none",
+    author_email="impulse-pw@openmailbox.org",
+    packages=["pylutim", "pylutim.lib"],
+    license="GPLv3",
+    keywords="pylutm, lutim, image, upload, api, warapper"
+    )
